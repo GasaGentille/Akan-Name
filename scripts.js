9594b048de4119be76626930f28492f8a1c44263
Rithaven <veneruth4@gmail.com>
2019-07-19 16:56:52 +0200

    Modify paragraphs

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ function day() {
   var MM = parseFloat(document.getElementById("month").value);
   var YY = parseFloat(document.getElementById("year").value);
   var CC = (YY - 1) / 100 + 1;
+
   var dayoftheweek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
   console.log(dayoftheweek);
   document.getElementById("result").innerHTML = Math.round(dayoftheweek);
@@ -17,8 +18,8 @@ function day() {
       document.getElementById("akan").innerHTML =maleNames[Math.round(dayoftheweek)];
   
   } else if (choice === "female") {
-  
       console.log(femaleNames[Math.round(dayoftheweek)]);
+      
       document.getElementById("akan").innerHTML = femaleNames[Math.round(dayoftheweek)];
   } else {
       console.log("invalid outuput");
