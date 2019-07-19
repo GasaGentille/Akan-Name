85351c3752376fa9227c0f19cb7309529bb4d9f0
Rithaven <veneruth4@gmail.com>
2019-07-19 10:33:38 +0200

    Add a new button

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function weekd() {
+  var d = parseFloat(document.getElementById('day').value);
+  var m = parseFloat(document.getElementById('month').value);
+  var y = parseFloat(document.getElementById('year').value);
+  var c = (y - 1) / 100 + 1;
+  var dayoftheweek = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
+  console.log(dayoftheweek);
+  document.getElementById('result').innerHTML = Math.round(dayoftheweek);
+  
+  var femaleNames= ["kwasi", "kwadwo", "kwabena", "kwaku", "yaw", "kofi", "kwame"];
+  var maleNames= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var choice = document.getElementById('select').value;
+  
+  if (choice === "male") {
+      console.log(femaleNames[Math.round(dayoftheweek)]);
+  
+      document.getElementById('akan').innerHTML =maleNames[Math.round(dayoftheweek)];
+  
+  } else if (choice === "female") {
+  
+      console.log(femaleNames[Math.round(dayoftheweek)]);
+      document.getElementById('akan').innerHTML = femaleNames[Math.round(dayoftheweek)];
+  } else {
+      console.log("invalid outuput");
+  }
