89bef4849e7f6ce14b76915afc10cb3aaa3f53c7
Rithaven <veneruth4@gmail.com>
2019-07-20 15:17:13 +0200

    modify README.md

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ function day() {
 
   /*variables that allow you to enter day,month and year */
 
-  var DD = parseFloat(document.getElementById("day").value);
-  var MM = parseFloat(document.getElementById("month").value);
-  var YY = parseFloat(document.getElementById("year").value);
+  var birthDay = parseFloat(document.getElementById("day").value);
+  var month = parseFloat(document.getElementById("month").value);
+  var year= parseFloat(document.getElementById("year").value);
 
   /* cc here is used to calculate the century */
 
-  var CC = (YY - 1) / 100 + 1;
+  var CC = (year - 1) / 100 + 1;
 
 /*weekday help you to calculate a day of week on which you were born on */
 
-  var weekDay = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
+  var weekDay = ( ( (CC/4) -2*CC-1) + ((5*year/4) ) + ((26*(month+1)/10)) + birthDay)%7;
   console.log( weekDay);
 
   /*to display a day of week on webpage */
@@ -29,12 +29,12 @@ function day() {
 /*to ckeck whether you are male or female */
 /*and check whether you've inserted  valid year,month and birth date  */
 
-if ((gender=== "male" && (DD>0 && DD<=31)&&(MM>0 && MM<=12)&& YY>=1)){
+if ((gender=== "male" && (birthDay>0 && birthDay<=31)&&(month>0 && month<=12)&& year>=1)){
     console.log(maleNames[parseInt( weekDay)]);
 
   document.getElementById("akan-name").innerHTML=maleNames[parseInt(weekDay)];
 
-} else if((gender==="female" && (DD>0 && DD<=31)&&(MM>0 && MM<=12)&& YY>=1)){
+} else if((gender==="female" && (birthDay>0 && birthDay<=31)&&(month>0 && month<=12)&& year>=1)){
    console.log(femaleNames[parseInt(weekDay)]);
 
 document.getElementById("akan-name").innerHTML= femaleNames[parseInt(weekDay)];
